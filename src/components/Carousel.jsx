d30226a9f36e1a6c2aef3d20a3091214ca5214e4
Parthssh <naturallyinsane13@gmail.com>
2024-01-09 14:27:25 +0530

    added react functionality

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,7 +6,68 @@ import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
-class Carousel extends Component {
+class Carousel extends React.Component {
+    constructor(){
+        super();
+        this.state =  {
+            index:0
+        }
+    
+    }
+
+
+   
+   
+   
+   
+   
+    render(){
+        let {index} = this.state;
+        let currentImage = images[index];
+
+        let handlePrev = () => {
+            this.setState({index:(this.state.index-1)% images.length})
+        }
+
+        let handleNext = () => {
+            this.setState({index:(this.state.index+1)% images.length})
+        }
+
+
+        return(
+            <>
+            <div className="mainContainer">
+                <div>
+                    <ArrowBackIosIcon onClick = {handlePrev} />
+                </div>
+
+                <div className="imageBox">
+                    <img src={currentImage.img} alt=" Display Images" />
+
+                </div>
+
+                <div className="imageContainer">
+                    <div className="titleBox">
+                        <p>{currentImage.title}</p>
+
+                    </div>
+
+                    <div className="subTitleBox">
+                        <p>{currentImage.subtitle}</p>
+
+                    </div>
+
+                </div>
+
+                <div>
+                    <ArrowForwardIosIcon  onClick = {handleNext}/>
+                </div>
+            </div>
+            </>
+        )
+    }
+
+
   
 }
 
